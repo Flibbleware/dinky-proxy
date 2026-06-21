c91ff3f8494a6d21fbdac7782774cd616c21d17c
Flibbleware <flibbleware@protonmail.com>
2026-06-21 14:51:59 +0100

    add suppression for createRoot no-non-null-assertion

--- apps/ui/src/main.tsx
+++ apps/ui/src/main.tsx
@@ -21,6 +21,7 @@ declare module '@tanstack/react-router' {
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
